0237b33f3de3c678657b466683f0fdeef4998e06
Kat Drobnjakovic <kat.drobnjakovic@gmail.com>
2014-02-18 10:28:53 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PetAdoption::Application.config.secret_key_base = '66721d476c34ec247367d9a4d3782c08a9ff954d10aa8a884a7108e9ca45e01851080c9d472cb52af08af6a69e5625529cdbfefc4b8b1ce1f4a9da6f8021ad85'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PetAdoption::Application.config.session_store :cookie_store, key: '_pet_adoption_session'
